0e4d25b10a4565e3371971e9dcef972fcbbe50cc
DamianAlv <77638063+DamianAlv@users.noreply.github.com>
2021-01-18 18:51:45 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ ! -f $HOME/.vnc/passwd ] ; then
+if [ ! -f home/.vnc/passwd ] ; then
 
     if  [ -z "$PASSWORD" ] ; then
         PASSWORD=`pwgen -c -n -1 12`
-        echo -e "PASSWORD = $PASSWORD" > $HOME/password.txt
+        echo -e "PASSWORD = $PASSWORD" > home/password.txt
     fi
 
     echo "$USER:$PASSWORD" | chpasswd
 
     # Set up vncserver
-    su $USER -c "mkdir $HOME/.vnc && echo '$PASSWORD' | vncpasswd -f > $HOME/.vnc/passwd && chmod 600 $HOME/.vnc/passwd && touch $HOME/.Xresources"
+    su $USER -c "mkdir home/.vnc && echo '$PASSWORD' | vncpasswd -f > home/.vnc/passwd && chmod 600 home/.vnc/passwd && touch home/.Xresources"
     chown -R $USER:$USER $HOME
 
     if [ ! -z "$SUDO" ]; then
@@ -22,7 +22,7 @@ if [ ! -f $HOME/.vnc/passwd ] ; then
 
 else
 
-    VNC_PID=`find $HOME/.vnc -name '*.pid'`
+    VNC_PID=`find home/.vnc -name '*.pid'`
     if [ ! -z "$VNC_PID" ] ; then
         vncserver -kill :1
         rm -rf /tmp/.X1*
@@ -33,10 +33,10 @@ fi
 if [ ! -z "$NGROK" ] ; then
         case "$NGROK" in
             [yY]|[yY][eE][sS])
-                su ubuntu -c "$HOME/ngrok/ngrok http 6080 --log $HOME/ngrok/ngrok.log --log-format json" &
+                su ubuntu -c "home/ngrok/ngrok http 6080 --log home/ngrok/ngrok.log --log-format json" &
                 sleep 5
                 NGROK_URL=`curl -s http://127.0.0.1:4040/status | grep -P "http://.*?ngrok.io" -oh`
-                su ubuntu -c "echo -e 'Ngrok URL = $NGROK_URL/vnc.html' > $HOME/ngrok/Ngrok_URL.txt"
+                su ubuntu -c "echo -e 'Ngrok URL = $NGROK_URL/vnc.html' > home/ngrok/Ngrok_URL.txt"
         esac
 fi
 
